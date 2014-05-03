a1db85a75757d9fff40163858477de81e80623b4
Gaëtan <Gatan@GAETANPORTABLE>
2014-05-03 14:35:51 +0200

    Ennemies et leurs animations

---
+++ src/Game/SmallEnnemy.java
@@ -0,0 +1,18 @@
+package Game;
+
+import org.mini2Dx.core.geom.Point;
+
+import Interface.AnimatorSmallEnnemy;
+
+public class SmallEnnemy extends Character {
+
+	public SmallEnnemy()
+	{
+		mouvement = new Move(new PointInt(0,200),new PointInt(0,200));
+		
+		// Init l'affichage de l'ennemi
+		animation = new AnimatorSmallEnnemy(new Point(0,200));
+		animation.create();
+				
+	}
+}
--- src/Interface/AnimatorSmallEnnemy.java
+++ src/Interface/AnimatorSmallEnnemy.java
@@ -9,11 +9,9 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.utils.Array;
 
-public class AnimatorPlayer extends Animator {
+public class AnimatorSmallEnnemy extends Animator {
 
-	private static int typeJoueur = 1; // Joueur 1 ou Joeur 2
-
-	public AnimatorPlayer(Point origin) {
+	public AnimatorSmallEnnemy(Point origin) {
 		super(origin);
 		// TODO Auto-generated constructor stub
 	}
@@ -22,46 +20,37 @@ public class AnimatorPlayer extends Animator {
 	 * initialisation de l'animation
 	 */
 	@Override
-	public void create() 
-	{
-		FRAME_COLS = 4;
-		FRAME_LINES = 3;
+	public void create() {
 		
-		if(typeJoueur == 1)
-		{
-			walkSheet = new Texture(Gdx.files.internal("res\\img\\J1\\1.png"));
-		}
-		else
-		{
-			walkSheet = new Texture(Gdx.files.internal("res\\img\\J2\\1.png"));
-		}
-
-		typeJoueur ++;
+		FRAME_COLS = 3;
+		FRAME_LINES = 2;
+		
+		walkSheet = new Texture(Gdx.files.internal("res\\img\\Ennemy\\1.png"));
 
 		TextureRegion[][] tmp = TextureRegion.split(walkSheet, walkSheet.getWidth()/FRAME_COLS, walkSheet.getHeight()/FRAME_LINES);
-
+		
 		walkFrames = new Array<TextureRegion>();
 		walkAnimation = new Animation[4];
-
+		
 		for (int j = 0; j < FRAME_COLS; j++) {
 			walkFrames.add(tmp[0][j]);
 		}
-		walkAnimation[0] = new Animation(0.20f, walkFrames);
+		walkAnimation[0] = new Animation(0.33f, walkFrames);
 		walkFrames.clear();
 
 		for (int j = 0; j < FRAME_COLS; j++) {
 			walkFrames.add(tmp[1][j]);
 		}
-		walkAnimation[1] = new Animation(0.20f, walkFrames);
+		walkAnimation[1] = new Animation(0.33f, walkFrames);
 		walkFrames.clear();
-
-		walkFrames.add(tmp[2][0]);
+		
+		walkFrames.add(tmp[0][0]);
 		walkAnimation[2] = new Animation(0f, walkFrames);
 		walkFrames.clear();
 
-		walkFrames.add(tmp[2][1]);
+		walkFrames.add(tmp[1][0]);
 		walkAnimation[3] = new Animation(0f, walkFrames);
-
+		
 		spriteBatch = new SpriteBatch();
 		stateTime = 0f;
 	}
